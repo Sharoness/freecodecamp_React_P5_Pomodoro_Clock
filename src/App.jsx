ed7bba9a4125106d1e8ad85c0da71e1a9860a5b4
Sharoness <sharong1234@hotmail.com>
2020-10-13 12:47:52 +0100

    changed increase and decrease so timercount changes with session/break + small changes in code

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 
 const App = () => {
-  const [breakLengthCount, setBreakLengthCount] = useState(6);
+  const [breakLengthCount, setBreakLengthCount] = useState(5);
   const [sessionLengthCount, setSessionLengthCount] = useState(25);
-  const [timerCount, setTimerCount] = useState(5); // sessionLengthCount*60
+  const [timerCount, setTimerCount] = useState(1500); 
   const [tellerId, setTellerId] = useState();
   const [sessionOrBreak, setSessionOrBreak] = useState("Session");
 
 
   useEffect(() => {
     if (timerCount < 0) {
+      console.log("useeffect tc", timerCount);
       clearInterval(tellerId);
       setTellerId(undefined);
       // playSound();
@@ -26,8 +27,8 @@ const App = () => {
     }
   }, [timerCount, tellerId])
 
-
   const startstop = () => {
+    console.log("startstop tc", timerCount);
     if (tellerId !== undefined) {
       clearInterval(tellerId);
       setTellerId(undefined);
@@ -40,7 +41,7 @@ const App = () => {
   const reset = () => {
     setBreakLengthCount(5);
     setSessionLengthCount(25);
-    setTimerCount(10); // 1500
+    setTimerCount(1500);
     clearInterval(tellerId);
     setTellerId(undefined);
     setSessionOrBreak("Session");
@@ -50,6 +51,11 @@ const App = () => {
     return (() => {
       if (tellerId === undefined && state < 60) {
         setter(state + 1);
+        if (sessionOrBreak === "Session") {
+          setTimerCount(sessionLengthCount*60 + 60);
+        } else {
+          setTimerCount(breakLengthCount*60 + 60);
+        }
       };
     });
   }
@@ -58,11 +64,18 @@ const App = () => {
     return (() => {
       if (tellerId === undefined && state > 1) {
         setter(state - 1);
+        setTimerCount(state*60);
+        if (sessionOrBreak === "Session") {
+          setTimerCount(sessionLengthCount*60 - 60);
+        } else {
+          setTimerCount(breakLengthCount*60 - 60);
+        }
       };
     });
   }
 
   const secToMmSs = (state) => {
+    console.log("sextommss tc", timerCount);
     const minutes = (state-state%60)/60;
     const seconds = state%60;
     if (minutes < 10 && seconds < 10) {
@@ -92,10 +105,10 @@ const App = () => {
       <button id="session-decrement" onClick={decrease(setSessionLengthCount, sessionLengthCount)}>session v</button>
       <button id="break-increment" onClick={increase(setBreakLengthCount, breakLengthCount)}>break ^</button>
       <button id="session-increment" onClick={increase(setSessionLengthCount, sessionLengthCount)}>session ^</button>
-      <div id="break-length">Break length: {breakLengthCount}</div>
-      <div id="session-length">Session length: {sessionLengthCount}</div>
-      <div id="timer-label">hi {sessionOrBreak}</div>
-      <div id="time-left">Time left: {secToMmSs(timerCount)}</div>
+      <p>Break length: </p><div id="break-length">{breakLengthCount}</div>
+      <p>Session length: </p><div id="session-length">{sessionLengthCount}</div>
+      <p>timerlabel: </p><div id="timer-label">{sessionOrBreak}</div>
+      <p>Time left: </p><div id="time-left">{secToMmSs(timerCount)}</div>
       <button id="start_stop" onClick={startstop}>start/stop</button>
       <button id="reset" onClick={reset}>reset</button>
     </div>
