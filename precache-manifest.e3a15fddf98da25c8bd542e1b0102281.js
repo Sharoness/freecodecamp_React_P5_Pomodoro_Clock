2e9ee6a6150bf6e1bd15fefffb21f29dd1ebbda0
Sharoness <sharong1234@hotmail.com>
2020-10-23 10:27:05 +0100

    Updates

---
+++ precache-manifest.e3a15fddf98da25c8bd542e1b0102281.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d163326b0a53e1cb0ec502d831fd669c",
+    "url": "/freecodecamp_React_P5_Pomodoro_Clock/index.html"
+  },
+  {
+    "revision": "b21c56332a520a3a7d3d",
+    "url": "/freecodecamp_React_P5_Pomodoro_Clock/static/css/main.f1ec8a65.chunk.css"
+  },
+  {
+    "revision": "14e6d76b6e9c83eafd4c",
+    "url": "/freecodecamp_React_P5_Pomodoro_Clock/static/js/2.e2d2024d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/freecodecamp_React_P5_Pomodoro_Clock/static/js/2.e2d2024d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b21c56332a520a3a7d3d",
+    "url": "/freecodecamp_React_P5_Pomodoro_Clock/static/js/main.801bb418.chunk.js"
+  },
+  {
+    "revision": "1e735accc31fe0f4f260",
+    "url": "/freecodecamp_React_P5_Pomodoro_Clock/static/js/runtime-main.e2007ec2.js"
+  }
+]);
